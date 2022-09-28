cbd95893bb21ba0a4f04fb228721b837f232061b
parvez13913 <parvezz13913@gmail.com>
2022-09-29 03:19:24 +0600

    personal information Section complete

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import developerImg from '../../images/developer/parvez.png';
-import resume from '../../images/resume/MERN Stack Resume.pdf';
-import { Link } from 'react-router-dom';
 import facebookIcon from '../../images/icon/facebook.svg';
 import instagramIcon from '../../images/icon/instagram.svg';
 import linkedinIcon from '../../images/icon/linkedin.svg';
@@ -13,30 +11,21 @@ const Banner = () => {
         <div className='pb-4 font'>
             <div className='lg:flex justify-around items-center'>
                 <div className='w-1/2'>
-                    <h1 className='text-5xl text-[#8cc090] font-bold mb-2'>HELLO!</h1>
-                    <h1 className='text-6xl font-bold my-2 text-white'>I Am,Parvez Rahman</h1>
-                    <p className='text-3xl text-[#808da4]'>
+                    <h1 className='text-4xl text-[#8cc090] font-bold my-3'>HELLO!</h1>
+                    <h1 className='text-5xl font-bold my-3 text-white'>I Am,Parvez Rahman</h1>
+                    <p className='text-2xl text-[#808da4] leading-relaxed'>
                         I'm Parvez Rahman, a professional web developer with long-time experience in this field​.
                     </p>
-                    <div className='flex items-center justify-start py-2'>
+                    <div className='flex items-center justify-start pt-5'>
                         <a target='_blank' className='mr-4 p-3 border rounded-full border-[#8cc090] hover:bg-[#8cc090]' href="https://www.facebook.com/parvez.rahman.13913"><img className='w-6' src={facebookIcon} alt="" /></a>
                         <a target='_blank' className='mx-5 p-3 border rounded-full border-[#8cc090] hover:bg-[#8cc090]' href="https://www.instagram.com/parvez13913/"><img className='w-5' src={instagramIcon} alt="" /></a>
                         <a target='_blank' className='mx-5 p-3 border rounded-full border-[#8cc090] hover:bg-[#8cc090]' href="https://www.linkedin.com/in/parvez-rahman-87714422a/"><img className='w-5' src={linkedinIcon} alt="" /></a>
                         <a target='_blank' className='mx-5 p-3 border rounded-full border-[#8cc090] hover:bg-[#8cc090]' href="https://github.com/parvez13913"><img className='w-5' src={githubIcon} alt="" /></a>
                     </div>
                     <button className="uppercase p-3 font-normal text-white btn-wide mt-6 border border-[#8cc090] hover:rounded-full transform duration-1000">
-                        <Link to={resume} target="_blank" download={resume}>
-                            <div className='flex items-center justify-center'>
-                                <span>
-                                    Download Resume
-                                </span>
-                                <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                        <path fillRule="evenodd" d="M10 3a.75.75 0 01.75.75v10.638l3.96-4.158a.75.75 0 111.08 1.04l-5.25 5.5a.75.75 0 01-1.08 0l-5.25-5.5a.75.75 0 111.08-1.04l3.96 4.158V3.75A.75.75 0 0110 3z" clipRule="evenodd" />
-                                    </svg>
-                                </span>
-                            </div>
-                        </Link>
+                        <a href='https://www.linkedin.com/in/parvez-rahman-87714422a/' target={'_blank'}>
+                            View Full Details
+                        </a>
                     </button>
                 </div>
                 <div className='lg:pt-3 pt-2'>
--- src/Pages/Home/PersonalDetails.js
+++ src/Pages/Home/PersonalDetails.js
@@ -1,21 +1,58 @@
 import React from 'react';
-import developerImg from '../../images/developer/parvezz.png';;
+import developerImg from '../../images/developer/parvezz.png';
+import resume from '../../images/resume/MERN Stack Resume.pdf';
+import { Link } from 'react-router-dom';
 
 const PersonalDetails = () => {
     return (
-        <div className='lg:flex justify-around items-center my-8 lg:container'>
-            <div className='w-[100%]'>
-                <img src={developerImg} alt="DeveloperPhoto" />
+        <div className='lg:flex justify-around items-center my-8 font'>
+            <div className='w-1/2'>
+                <img className='w-96 rounded-md hover:scale-95 hover:-rotate-1 transform duration-1000' src={developerImg} alt="developerImg" />
             </div>
-            <div className='text-center mt-16 lg:w-[50%]'>
-                <h3 className='my-8'>About Me</h3>
-                <h1 className='text-3xl font-bold'>PERSONAL DETAILS</h1>
-                <p className='mt-8'>
-                    Here, I focus on a range of items and features that we use in life without giving them a second thought. such as web development.
-                </p>
-                <a href='https://www.linkedin.com/in/parvez-rahman-87714422a/' target={'_blank'}>
-                    <button className="bg-gradient-to-r from-cyan-500 to-blue-500 uppercase p-3 font-semibold text-white lg:hover:bg-primary btn-wide mt-6">View Full Details</button>
-                </a>
+            <div className='text-left mt-16'>
+                <div>
+                    <h3 className='text-4xl mt-2 font-bold text-[#8cc090]'>About Me</h3>
+                </div>
+                <div>
+                    <h2 className='text-3xl font-semibold mt-4 text-white'>PERSONAL INFORMATION</h2>
+                    <div className='grid grid-cols-2 gap-4 my-4 text-[#808da4]'>
+                        <p>
+                            <span className='font-semibold'>Name:</span> Parvez Rahman
+                        </p>
+                        <p>
+                            <span className='font-semibold'>Age:</span> 22 Years
+                        </p>
+                        <p>
+                            <span className='font-semibold'>Email:</span> parvezz13913@gmail.com
+                        </p>
+                        <p>
+                            <span className='font-semibold'>Address:</span> Jhenaidah,Bangladesh
+                        </p>
+                        <p>
+                            <span className='font-semibold'>Skill:</span> Font-End
+                        </p>
+                        <p>
+                            <span className='font-semibold'>Experience:</span> 1 Yerars
+                        </p>
+                        <p>
+                            <span className='font-semibold'>Language:</span> Bangla,English
+                        </p>
+                    </div>
+                </div>
+                <button className="uppercase p-3 font-normal text-white btn-wide mt-6 border border-[#8cc090] hover:rounded-full transform duration-1000">
+                    <Link to={resume} target="_blank" download={resume}>
+                        <div className='flex items-center justify-center'>
+                            <span>
+                                Download Resume
+                            </span>
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                                    <path fillRule="evenodd" d="M10 3a.75.75 0 01.75.75v10.638l3.96-4.158a.75.75 0 111.08 1.04l-5.25 5.5a.75.75 0 01-1.08 0l-5.25-5.5a.75.75 0 111.08-1.04l3.96 4.158V3.75A.75.75 0 0110 3z" clipRule="evenodd" />
+                                </svg>
+                            </span>
+                        </div>
+                    </Link>
+                </button>
             </div>
         </div>
     );
