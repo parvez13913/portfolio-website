625ca8719e63e3b57a999928b50edaed6c93a036
parvez13913 <parvezz13913@gmail.com>
2023-10-27 10:00:33 +0600

    [DONE] custom link

--- src/Pages/Home/Skill.js
+++ src/Pages/Home/Skill.js
@@ -5,11 +5,14 @@ import tailwind from '../../images/skill/tailwind.svg';
 import bootstrap from '../../images/skill/bootstrap.svg';
 import javascript from '../../images/skill/javascript.svg';
 import react from '../../images/skill/react.svg';
+import redux from '../../images/skill/redux-logo.svg';
+import nextAuth from '../../images/skill/next-auth.png';
 import mongoDB from '../../images/skill/mongoDb.png';
 import firebase from '../../images/skill/firebase.svg';
 import typescript from '../../images/skill/typescript.svg';
 import nextJS from '../../images/skill/nextJS.svg';
 import expressJs from '../../images/skill/expressJs.svg';
+import mongoose from '../../images/skill/mongoose.png';
 import nodeJs from '../../images/skill/nodeJs.svg';
 import figma from '../../images/skill/figma.svg';
 import canva from '../../images/skill/canva.svg';
@@ -19,16 +22,19 @@ const Skill = () => {
     const skills = [
         { name: "HTML", img: html, id: 153637383930 },
         { name: "CSS", img: css, id: 20987656544332 },
-        { name: "Tailwind", img: tailwind, id: 4123456768 },
         { name: "Bootstrap", img: bootstrap, id: 5234567890 },
+        { name: "Tailwind", img: tailwind, id: 4123456768 },
         { name: "Javascript", img: javascript, id: 623456798 },
-        { name: "React", img: react, id: 30987654 },
-        { name: "MongoDB", img: mongoDB, id: 7223445778990 },
-        { name: "firebase", img: firebase, id: 83345567 },
         { name: "Typescript", img: typescript, id: 933445566778989 },
+        { name: "React", img: react, id: 30987654 },
         { name: "Next", img: nextJS, id: 1033456778 },
-        { name: "Express", img: expressJs, id: 114567788 },
+        { name: "Next Auth", img: nextAuth, id: 103345677877665 },
+        { name: "Redux", img: redux, id: 1033456778345 },
+        { name: "firebase", img: firebase, id: 83345567 },
         { name: "Node", img: nodeJs, id: 1222334333 },
+        { name: "Express", img: expressJs, id: 114567788 },
+        { name: "MongoDB", img: mongoDB, id: 7223445778990 },
+        { name: "Mongoose", img: mongoose, id: 34457789334290 },
         { name: "figma", img: figma, id: 134444444444 },
         { name: "canva", img: canva, id: 14775858585 }
     ]
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,67 +1,56 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CustomLink from '../../CustomLink/CustomLink';
 
 
 const Header = () => {
     return (
         <div className="navbar bg-[#152136] sticky top-0 z-50">
+            <div>
+                <h5 className='text-lg text-[#8b98af]'>Parvez</h5>
+            </div>
             <div className="text-[#8b98af] mx-auto">
-                <ul className="p-0 font-semibold flex space-x-11 ">
+                <ul className="p-0 font-semibold flex space-x-8">
                     <li>
-                        <div>
-                            <Link className='flex flex-col items-center' to='/home'>
-                                <h6>Home</h6>
-                                <i className="fa-solid fa-house"></i>
-                            </Link>
-                        </div>
+                        <CustomLink className="space-x-2 btn" to='/home'>
+                            <i className="fa-solid fa-house"></i>
+                            <span>Home</span>
+                        </CustomLink>
                     </li>
                     <li>
-                        <div>
-                            <Link className='flex flex-col items-center' to='/about'>
-                                <h6>About</h6>
-                                <i className="fa-solid fa-user"></i>
-                            </Link>
-                        </div>
+                        <CustomLink className='space-x-2 btn' to='/about'>
+                            <i className="fa-solid fa-user"></i>
+                            <span>About</span>
+                        </CustomLink>
                     </li>
                     <li>
-                        <div>
-                            <Link className='flex flex-col items-center' to='/skill'>
-                                <h6>Skills</h6>
-                                <i className="fa-solid fa-lightbulb"></i>
-                            </Link>
-                        </div>
+                        <CustomLink className='space-x-2 btn' to='/skill'>
+                            <i className="fa-solid fa-lightbulb"></i>
+                            <span>Skills</span>
+                        </CustomLink>
                     </li>
                     <li>
-                        <div>
-                            <Link className='flex flex-col items-center' to='/education'>
-                                <h6>Education</h6>
-                                <i className="fa-solid fa-user-graduate"></i>
-                            </Link>
-                        </div>
+                        <CustomLink className='space-x-2 btn' to='/education'>
+                            <i className="fa-solid fa-user-graduate"></i>
+                            <span>Education</span>
+                        </CustomLink>
                     </li>
                     <li>
-                        <div>
-                            <Link className='flex flex-col items-center' to='/service'>
-                                <h6>Services</h6>
-                                <i className="fa-solid fa-gears"></i>
-                            </Link>
-                        </div>
+                        <CustomLink className='space-x-2 btn' to='/service'>
+                            <i className="fa-solid fa-gears"></i>
+                            <span>Services</span>
+                        </CustomLink>
                     </li>
                     <li>
-                        <div>
-                            <Link className='flex flex-col items-center' to='/projects'>
-                                <h6>Projects</h6>
-                                <i className="fa-solid fa-briefcase"></i>
-                            </Link>
-                        </div>
+                        <CustomLink className='space-x-2 btn' to='/projects'>
+                            <i className="fa-solid fa-briefcase"></i>
+                            <span>Projects</span>
+                        </CustomLink>
                     </li>
                     <li>
-                        <div>
-                            <Link className='flex flex-col items-center' to='/contact'>
-                                <h6>Contact</h6>
-                                <i className="fa-solid fa-address-book"></i>
-                            </Link>
-                        </div>
+                        <CustomLink className='space-x-2 btn' to='/contact'>
+                            <i className="fa-solid fa-address-book"></i>
+                            <span>Contact</span>
+                        </CustomLink>
                     </li>
                 </ul>
             </div>
